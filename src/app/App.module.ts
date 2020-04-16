52c349050be2beaa7f52dc98b23a32cf56cc7701
Lars <lars.bs@hotmail.com>
2020-04-16 11:33:02 +0200

    Remove comments

--- src/app/App.module.ts
+++ src/app/App.module.ts
@@ -6,38 +6,12 @@ import { About } from './routes/About';
 import { Root } from './routes/Root';
 
 @module({
-  /**
-   * It's not the same as importing a module, as import/export is not related
-   * with routes. In this case, even if you tried to use one of the exports
-   * from AuthModule or DashboardModule in one of the hipothetical providers
-   * of this module, it won't work because they're not imported first.
-   *
-   * However, the app would render normally in this case since we don't need
-   * any of the exported providers from AuthModule or DashboardModule.
-   */
   routing: [
     createRoute('*', Root, [
       includeRoutes('/auth', AuthModule),
       includeRoutes('/', DashboardModule),
     ]),
     createRoute('/about', About),
-    /**
-     * It's also possible to nest createRoutes
-     *
-     * Either without root view (just prefixes the children):
-     *
-     * createRoute('/admin', [
-     *   createRoute('/', AdminHome),
-     *   createRoute('/users', AdminUsers),
-     * ]),
-     *
-     * Or having a root view (necessary to use <Outlet />)
-     *
-     * createRoute('/admin', Admin, [
-     *   createRoute('/', AdminHome),
-     *   createRoute('/users', AdminUsers),
-     * ]);
-     */
   ],
 })
 export class AppModule {}
