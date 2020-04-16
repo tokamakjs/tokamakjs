86daf1c68523fc2fcbff7d00ee15df998d85c7ef
Lars <lars.bs@hotmail.com>
2020-04-16 11:42:21 +0200

    More visual improvements and migrate more comments to github issues

--- src/app/routes/Root/Root.view.tsx
+++ src/app/routes/Root/Root.view.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { Outlet } from 'react-router-dom';
 
 import { Navbar } from '~/components/Navbar';
 
 export const RootView = () => {
-  // Here is a good place to put all the providers
-  // necessary for the app as well.
   return (
-    <div>
+    <Fragment>
       <Navbar />
-      <p>
-        <Link to="/about">Go to about</Link>
-      </p>
-      <p>
-        <Link to="/auth/login">Go to login</Link>
-      </p>
-      {/* This outlet will render children routes */}
       <Outlet />
-      {/* It's also possible to pass data to children using the data prop */}
-      {/* <Outlet data={{ hello: 'world' }} /> */}
-    </div>
+    </Fragment>
   );
 };
