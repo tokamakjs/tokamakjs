47739928972eb4a7be8f56b7c73e6e3f99557e66
Lars Ruiz <lars.bs@hotmail.com>
2020-06-12 16:23:13 +0200

    Small changes to interfaces

--- packages/core/src/interfaces/controller-life-cycle.ts
+++ packages/core/src/interfaces/controller-life-cycle.ts
@@ -2,20 +2,20 @@ export interface OnDidMount {
   onDidMount(): void | Promise<void> | VoidFunction;
 }
 
-export function hasOnDidMount(value: any): value is OnDidMount {
-  return value?.onDidMount != null;
-}
-
 export interface OnWillUnmount {
   onWillUnmount(): void;
 }
 
-export function hasOnDidUnmount(value: any): value is OnWillUnmount {
-  return value?.onDidUnmount != null;
-}
-
 export interface OnDidRender {
   onDidRender(): void;
+}
+
+export function hasOnDidMount(value: any): value is OnDidMount {
+  return value?.onDidMount != null;
+}
+
+export function hasOnDidUnmount(value: any): value is OnWillUnmount {
+  return value?.onDidUnmount != null;
 }
 
 export function hasOnDidRender(value: any): value is OnDidRender {
--- packages/core/src/interfaces/guards.ts
+++ packages/core/src/interfaces/guards.ts
@@ -1,5 +1,7 @@
 import { RouterState } from './router-state';
 
+export type Scheduler = (fn: Function) => void;
+
 export interface CanActivate {
-  canActivate(state: RouterState, schedule: (fn: Function) => void): boolean | Promise<boolean>;
+  canActivate(state: RouterState, schedule: Scheduler): boolean | Promise<boolean>;
 }
