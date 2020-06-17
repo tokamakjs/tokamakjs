4f3417e70700ed85e0da240fc20a493e69fac983
Lars Ruiz <lars.bs@hotmail.com>
2020-06-17 15:12:22 +0200

    Delete unused enum

--- packages/core/src/injection/enums.ts
+++ packages/core/src/injection/enums.ts
@@ -1,10 +1,3 @@
-export enum ModuleMetadataKeys {
-  IMPORTS = 'imports',
-  PROVIDERS = 'providers',
-  ROUTING = 'routing',
-  EXPORTS = 'exports',
-}
-
 export enum Scope {
   SINGLETON = 'SINGLETON',
   TRANSIENT = 'TRANSIENT',
