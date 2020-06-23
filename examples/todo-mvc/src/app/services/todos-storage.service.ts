969da773cafb5a2d292dc1847271152d193c1428
Lars Ruiz <lars.bs@hotmail.com>
2020-06-23 13:30:32 +0200

    Change filters key

--- examples/todo-mvc/src/app/services/todos-storage.service.ts
+++ examples/todo-mvc/src/app/services/todos-storage.service.ts
@@ -5,14 +5,14 @@ import { Todo } from '~/stores';
 @injectable()
 export class TodosStorageService {
   public persistTodos(todos: Array<Todo>): void {
-    localStorage.setItem('TodoMVC-Todos', JSON.stringify(todos));
+    localStorage.setItem('todos-tokamak', JSON.stringify(todos));
   }
 
   public readTodos(): Array<Todo> {
     try {
-      return JSON.parse(localStorage.getItem('TodoMVC-Todos') ?? '[]');
+      return JSON.parse(localStorage.getItem('todos-tokamak') ?? '[]');
     } catch {
-      localStorage.removeItem('TodoMVC-Todos');
+      localStorage.removeItem('todos-tokamak');
       return [];
     }
   }
