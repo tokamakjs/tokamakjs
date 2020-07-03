fb0ebf050edd08f947381943585040ef6dcf285b
Lars Ruiz <lars.bs@hotmail.com>
2020-07-03 16:56:34 +0200

    wip

--- packages/cli/src/commands/new/new-action.ts
+++ packages/cli/src/commands/new/new-action.ts
@@ -1,9 +1,7 @@
 import path from 'path';
 
 import execa from 'execa';
-import figures from 'figures';
 import fs from 'fs-extra';
-import ora from 'ora';
 
 const TEMPLATE_DIR = path.resolve(__dirname, 'template');
 
@@ -13,11 +11,19 @@ const dependencies = [
   '@tokamakjs/core',
   'react',
   'react-dom',
+  'modern-normalize',
 ];
 
 const devDependencies = ['@types/react', '@types/react-dom', 'prettier', 'rimraf', 'typescript'];
 
-async function _install(cwd: string, deps: Array<string>, dev: boolean) {
+async function _checkCanCreate(cwd: string): Promise<boolean> {
+  const conflictingFiles = ['package.json'];
+  await fs.ensureDir(cwd);
+  const files = await fs.readdir(cwd);
+  return files.every((f) => !conflictingFiles.includes(f));
+}
+
+async function _install(cwd: string, deps: Array<string>, dev: boolean): Promise<void> {
   await execa(
     'npm',
     ['install', dev ? '--save-dev' : '--save', '--save-exact', '--loglevel', 'error', ...deps],
@@ -28,17 +34,28 @@ async function _install(cwd: string, deps: Array<string>, dev: boolean) {
 export async function newAction(name: string) {
   const outputDir = path.resolve(process.cwd(), name);
 
-  const spinner = ora();
+  if (!(await _checkCanCreate(outputDir))) {
+    process.stdout.write(
+      'The target directory seems to contain a project already. Try using a different name or removing the folder first.\n',
+    );
+    process.exit(1);
+  }
 
-  spinner.start('Copying files into destination...');
+  process.stdout.write(`Creating a Tokamak app in ${outputDir}...\n`);
+
+  process.stdout.write('Copying files into destination...\n');
   await fs.copy(TEMPLATE_DIR, outputDir);
-  spinner.stopAndPersist({
-    text: 'Finished copying files into destination.',
-    symbol: figures.tick,
-  });
 
   process.stdout.write('Installing dependencies...\n');
   await _install(outputDir, dependencies, false);
+
   process.stdout.write('Installing devDependencies...\n');
   await _install(outputDir, devDependencies, true);
+
+  process.stdout.write(`App created succesfully in ${outputDir}\n`);
+  process.stdout.write(`Inside the newly created directory, you can run:\n`);
+  process.stdout.write(`  - npm start\n`);
+  process.stdout.write(`    Starts the app in development mode.\n`);
+  process.stdout.write(`  - npm run build\n`);
+  process.stdout.write(`    Builds the app for deployment.\n`);
 }
