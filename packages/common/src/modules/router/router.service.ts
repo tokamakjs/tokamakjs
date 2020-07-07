b2ff28b09f51a7d2059b40c2fcac9b1d91059d2a
Lars Ruiz <lars.bs@hotmail.com>
2020-07-07 16:13:10 +0200

    Add query to router service

--- packages/common/src/modules/router/router.service.ts
+++ packages/common/src/modules/router/router.service.ts
@@ -1,15 +1,25 @@
 import { HISTORY, inject, injectable } from '@tokamakjs/core';
 import { History } from 'history';
+import qs from 'query-string';
 
 @injectable()
 export class RouterService {
-  constructor(@inject(HISTORY) private readonly history: History) {}
+  constructor(@inject(HISTORY) private readonly _history: History) {}
+
+  get query() {
+    const { search } = this._history.location;
+    return qs.parse(search);
+  }
+
+  get rawQuery() {
+    return this._history.location.search;
+  }
 
   public push(...args: Parameters<History['push']>): void {
-    this.history.push(...args);
+    this._history.push(...args);
   }
 
   public replace(...args: Parameters<History['replace']>): void {
-    this.history.replace(...args);
+    this._history.replace(...args);
   }
 }
