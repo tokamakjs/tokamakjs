e167575cb05d370a8a2a7686e02a9dad72def1f1
Lars Ruiz <lars.bs@hotmail.com>
2020-07-08 13:10:53 +0200

    Improve event emitter typing

--- packages/common/src/utils/event-emitter.ts
+++ packages/common/src/utils/event-emitter.ts
@@ -1,26 +1,28 @@
 export type Listener<T> = (event: T) => void;
 
 export abstract class EventEmitter<T> {
-  private _listenersByKey = {} as Record<keyof T, Array<Listener<any>>>;
+  private _listenersByKey: {
+    [K in keyof T]?: Array<Listener<T[K]>>;
+  } = {};
 
   public addEventListener<K extends keyof T>(key: K, listener: Listener<T[K]>): void {
     if (this._listenersByKey[key] == null) {
       this._listenersByKey[key] = [listener];
     } else {
-      this._listenersByKey[key].push(listener);
+      this._listenersByKey[key]!.push(listener);
     }
   }
 
   public removeEventListener<K extends keyof T>(key: K, listener: Listener<T[K]>): void {
-    this._listenersByKey[key] = this._listenersByKey[key].filter((l) => l !== listener);
+    this._listenersByKey[key] = this._listenersByKey[key]?.filter((l) => l !== listener);
   }
 
   protected emit<K extends keyof T>(key: K, event: T[K]): void {
-    const eventListeners = this._listenersByKey[key];
+    const eventListeners = this._listenersByKey[key] ?? [];
     eventListeners.forEach((l) => l(event));
   }
 
   public removeEventListeners(): void {
-    this._listenersByKey = {} as Record<keyof T, Array<Listener<any>>>;
+    this._listenersByKey = {};
   }
 }
