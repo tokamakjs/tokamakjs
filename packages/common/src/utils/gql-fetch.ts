7b8f9f502f2987b780c0e79a695122ce2bd7a9d6
Lars Ruiz <lars.bs@hotmail.com>
2020-07-15 13:16:29 +0200

    Add gql-fetch

---
+++ packages/common/src/utils/gql-fetch.ts
@@ -0,0 +1,21 @@
+import { ASTNode, print } from 'graphql';
+
+interface GqlFetchArgs<V> {
+  url: string;
+  query: ASTNode;
+  variables: V;
+  headers: Record<string, string>;
+}
+
+export async function gqlFetch<V>({
+  url,
+  query,
+  variables,
+  headers,
+}: GqlFetchArgs<V>): Promise<Response> {
+  return await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json', ...headers },
+    body: JSON.stringify({ query: print(query), variables }),
+  });
+}
--- packages/common/src/utils/index.ts
+++ packages/common/src/utils/index.ts
@@ -2,3 +2,4 @@ export * from './run';
 export * from './noop';
 export * from './delay';
 export * from './event-emitter';
+export * from './gql-fetch';
