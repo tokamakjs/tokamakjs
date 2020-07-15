43dea334d18488cea11a37f59ddb33a65a93fb3d
Lars Ruiz <lars.bs@hotmail.com>
2020-07-15 17:47:54 +0200

    Export forwardRef

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -5,3 +5,4 @@ export * from './routing/utils';
 export * from './interfaces';
 export * from './routing/hooks';
 export * from './routing/router/components';
+export { forwardRef } from './injection';
