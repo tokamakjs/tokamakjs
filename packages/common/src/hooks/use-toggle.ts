d6592effd24fe0ef6ecdee721355e6de113ecda1
Lars Ruiz <lars.bs@hotmail.com>
2021-04-28 10:15:05 +0200

    set default for useToggle

--- packages/common/src/hooks/use-toggle.ts
+++ packages/common/src/hooks/use-toggle.ts
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export function useToggle(defaults: boolean): [value: boolean, toggler: VoidFunction] {
+export function useToggle(defaults: boolean = false): [value: boolean, toggler: VoidFunction] {
   const [value, setValue] = useState(defaults);
   return [value, () => setValue(!value)];
 }
