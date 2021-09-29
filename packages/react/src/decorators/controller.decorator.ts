db072775aa5738f7722cfd9187f0fc5d16ceac01
Bartolomeu <pbartmr@gmail.com>
2021-09-29 14:19:45 +0100

    Add hot reload to injectables and controllers

--- packages/react/src/decorators/controller.decorator.ts
+++ packages/react/src/decorators/controller.decorator.ts
@@ -1,8 +1,24 @@
 import { Reflector } from '../reflection';
 import { ControllerMetadata } from '../types';
 
+const previousPrototypes = new Map()
+
 export function Controller(metadata: ControllerMetadata): ClassDecorator {
   return (Target: Function) => {
+    if(module.hot) {
+      const previousPrototype = previousPrototypes.get(Target.name)
+
+      if(previousPrototype) {
+        Object.getOwnPropertyNames(Target.prototype).forEach((k) => {
+          const value = Target.prototype[k]
+
+          previousPrototype[k] = value;
+        })
+      }
+
+      previousPrototypes.set(Target.name, Target.prototype)
+    }
+
     Reflector.addControllerMetadata(Target, metadata);
 
     const proxy = new Proxy(Target, {
